6a7157b8b8405fd68de35526ec77d2467f18ad5a
chabikun <yu_ichiro_m@yahoo.co.jp>
2019-02-21 23:08:49 +0900

    ID名の修正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -7,7 +7,7 @@ $(function() {
     return html
   }
   function buildHTML_add_candidacy(user_id, user_name){
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user'>
                   <input name='group[user_ids][]' type='hidden' value=${ user_id }>
                   <p class='chat-group-user__name'>${ user_name }</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
