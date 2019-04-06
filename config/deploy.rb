063da1dd142e013fb947dcbd3828fb0682cd8c39
chabikun <yu_ichiro_m@yahoo.co.jp>
2019-04-06 19:21:49 +0900

    修正3

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/cha0714bi.pem']
+                  keys: ['~/.ssh/id_rsa']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
