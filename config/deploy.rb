dc4690bd046d7e8b96fd6c4c1715d9d936686468
chabikun <yu_ichiro_m@yahoo.co.jp>
2019-04-06 19:09:00 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,20 @@ set :default_env, {
 }
 
 set :linked_files, %w{ config/secrets.yml }
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/cha0714bi.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
